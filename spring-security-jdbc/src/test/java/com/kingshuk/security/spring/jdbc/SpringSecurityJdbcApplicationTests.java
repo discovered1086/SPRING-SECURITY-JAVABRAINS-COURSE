b768dabf210bae1f28c7e47ff0ea292fc11b857b
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-12-03 20:13:07 -0600

    Initial commit

---
+++ spring-security-jdbc/src/test/java/com/kingshuk/security/spring/jdbc/SpringSecurityJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingshuk.security.spring.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
