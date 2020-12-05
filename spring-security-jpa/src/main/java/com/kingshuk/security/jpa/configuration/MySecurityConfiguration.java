0dabc5990e1602ca03cdfbc01fec845bed4023ac
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-12-05 10:27:08 -0600

    jpa project created

---
+++ spring-security-jpa/src/main/java/com/kingshuk/security/jpa/configuration/MySecurityConfiguration.java
@@ -0,0 +1,11 @@
+package com.kingshuk.security.jpa.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class MySecurityConfiguration {
+
+
+}
