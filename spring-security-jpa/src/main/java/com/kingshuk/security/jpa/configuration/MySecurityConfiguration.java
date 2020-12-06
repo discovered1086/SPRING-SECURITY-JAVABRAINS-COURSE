66fd0b5b7489c3622b704b72efde2c67f237099e
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-12-05 19:22:44 -0600

    Hard coded user details service implementation

--- spring-security-jpa/src/main/java/com/kingshuk/security/jpa/configuration/MySecurityConfiguration.java
+++ spring-security-jpa/src/main/java/com/kingshuk/security/jpa/configuration/MySecurityConfiguration.java
@@ -2,12 +2,15 @@ package com.kingshuk.security.jpa.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
@@ -29,5 +32,10 @@ public class MySecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/h2-console", "/").permitAll()
                 .and().formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
--- spring-security-jpa/src/main/java/com/kingshuk/security/jpa/service/MyUserDetailsService.java
+++ spring-security-jpa/src/main/java/com/kingshuk/security/jpa/service/MyUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.kingshuk.security.jpa.service;
 
+import com.kingshuk.security.jpa.model.MyUserDetails;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,6 +10,6 @@ import org.springframework.stereotype.Service;
 public class MyUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return new MyUserDetails(username);
     }
 }
