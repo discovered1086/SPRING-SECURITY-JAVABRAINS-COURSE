2b9a2dc7c6aa42c64d0be2cda4ebb598fa57b208
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-12-05 21:15:44 -0600

    Improved user details service #2

--- spring-security-jpa/src/main/java/com/kingshuk/security/jpa/service/MyUserDetailsService.java
+++ spring-security-jpa/src/main/java/com/kingshuk/security/jpa/service/MyUserDetailsService.java
@@ -5,16 +5,12 @@ import com.kingshuk.security.jpa.model.UserDetailsRepository;
 import com.kingshuk.security.jpa.model.entity.UserProfile;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service("myUserDetailsService")
@@ -22,19 +18,13 @@ import java.util.stream.Collectors;
 @Slf4j
 public class MyUserDetailsService implements UserDetailsService {
 
-    private UserDetailsRepository userDetailsRepository;
+    private final UserDetailsRepository userDetailsRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<UserProfile> userProfile = userDetailsRepository.findByUserName(username);
+        UserProfile profile = userDetailsRepository.findByUserName(username).orElseThrow(() -> new UsernameNotFoundException("User with the user name not found...."));
 
-        if (!userProfile.isPresent()) {
-            throw new UsernameNotFoundException("User with the user name not found....");
-        }
-
-        UserProfile profile = userProfile.get();
-
-        log.info("The user profile: "+userProfile);
+        log.info("The user profile: " + profile);
 
         MyUserDetails myUserDetails = MyUserDetails.builder()
                 .userName(profile.getUserName())
@@ -44,7 +34,7 @@ public class MyUserDetailsService implements UserDetailsService {
 
         myUserDetails.setRoles(profile.getRoles().stream()
                 .map(role -> new SimpleGrantedAuthority(role.getRole()))
-        .collect(Collectors.toSet()));
+                .collect(Collectors.toSet()));
 
         return myUserDetails;
     }
